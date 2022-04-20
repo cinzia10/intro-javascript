2087d617ce1d918790bd97e8be75828a8848515d
LittleAkira <cinzia.ariotti.10@gmail.com>
2022-04-20 10:07:41 +0200

    create file

---
+++ script.js
@@ -0,0 +1,65 @@
+// string
+
+console.log('Ciao mondo!');
+
+// alert('Ciao mondo!');
+
+console.log('Ciao Cinzia');
+
+console.log('7')
+
+console.log('Ciao' + 'Andrea')
+
+// number
+
+console.log(7);
+
+console.log('somma', 10 + 5);
+
+console.log('sottrazione', 10 - 5);
+
+console.log('moltiplicazione', 10 * 5);
+
+console.log('divisione', 10 / 5);
+
+console.log('potenza', 5 ** 2);
+
+console.log('modulo', 5 % 2);
+
+// library Math
+
+console.log("Math", Math.round(3.48))
+
+// complicazioni
+
+console.log("Ciao" + 7);
+
+console.log("7" + 7);
+
+console.log(7 + "7");
+
+//boolean
+
+console.log(false);
+
+console.log(true);
+
+console.log('not true', !true);
+
+console.log('not false', !false);
+
+console.log('true and true',true&&true);
+
+console.log('true and false',true&&false);
+
+console.log('false and true',false&&true);
+
+console.log('false and false',false&&false);
+
+console.log('true or false', true||false);
+
+console.log('true or true', true||true);
+
+console.log('false or false', false||false);
+
+console.log('false or true', false||true);
