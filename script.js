b4d8e644c4939e4a019800248cc9105f9f5a84ee
LittleAkira <cinzia.ariotti.10@gmail.com>
2022-04-20 11:02:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -41,53 +41,40 @@ console.log(7 + "7");
 //boolean
 
 console.log(false);
-
 console.log(true);
 
 // funzione logica NOT
 
 console.log('NOT true', !true);
-
 console.log('NOT false', !false);
 
 // funzione logica AND
 
 console.log('true AND true',true&&true);
-
 console.log('true AND false',true&&false);
-
 console.log('false AND true',false&&true);
-
 console.log('false AND false',false&&false);
 
 // funzione logica OR
 
 console.log('true OR true', true||true);
-
 console.log('true OR false', true||false);
-
 console.log('false OR true', false||true);
-
 console.log('false OR false', false||false);
 
 //funzione logica EQUAL
 
 console.log('true EQUAL true', true===true);
-
 console.log('true EQUAL false', true===false);
-
 console.log('false EQUAL true', false===true);
-
 console.log('false EQUAL false', false===false);
 
 // funzione logica EQUAL altre applicazioni
 
 console.log("Pippo EQUAL Pippo", 'Pippo' === 'Pippo');
-
 console.log("Pippo EQUAL Pluto", 'Pippo' === 'Pluto');
 
 console.log("7 EQUAL 7", 7===7);
-
 console.log("8 EQUAL 2", 8===2);
 
 // complicazioni
@@ -96,3 +83,20 @@ console.log("'7' === 7", '7' === 7);
 
 console.log("'7' == 7", '7'==7);
 
+// variabile
+
+ let pippo = 7;
+
+ console.log(pippo);
+
+ console.log(pippo + 3);
+
+ let pluto = 5;
+
+ console.log(pippo + pluto);
+
+ let paperino = 'Ciao';
+
+ console.log(paperino);
+
+ console.log(paperino + pluto);
