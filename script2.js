b088e270ed736a8f7ff955b7a264af2334a102b4
LittleAkira <cinzia.ariotti.10@gmail.com>
2022-04-20 11:43:17 +0200

    add script2

---
+++ script2.js
@@ -0,0 +1,12 @@
+//condizionali
+const stringNumber = prompt('inserisci un numero');
+
+const number = parseInt(StringNumber);
+
+const isEven = number % 2 === 0;
+
+if (isEven) {
+    console.log('il numero è pari');
+} else {
+    console.log('il numero è dispari')
+}
