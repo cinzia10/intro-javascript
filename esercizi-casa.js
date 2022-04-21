da35fa6ed6a9959672fee82745a70fc9e36dbf82
cinzia10 <103512084+cinzia10@users.noreply.github.com>
2022-04-21 17:57:42 +0200

    Add files via upload

---
+++ esercizi-casa.js
@@ -0,0 +1,87 @@
+//scrivere un ciclo for che scriva a console i numeri da 10 a 50
+
+// for (let i = 10; i <= 50; i++) {
+//     console.log(i)
+// }
+
+//scrivere un ciclo for che scriva a console i numeri pari da 8 a 128
+
+// for (let i = 8; i < 130; i++) {
+//     if (i % 2 ===0) {
+//         console.log(i);
+//     }
+    
+// }
+
+//scrivere un ciclo for che scriva a console i numeri da 100 a 0
+
+// for (let i = 100; i >= 0; i--) {
+//     console.log(i)
+    
+// }
+
+//scrivere un ciclo for che sommi i numeri da 0 a 100
+
+
+
+//ripetere gli stessi esercizi con il ciclo while e do...while
+
+// ESERCIZIO 1
+
+// let numero = 10;
+
+// while (numero < 51) {
+//     console.log(numero);
+//     numero = numero + 1;
+// }
+
+
+// let numero = 10;
+
+// do {
+//     console.log(numero);
+//     numero = numero + 1;
+// } while (numero <= 50);
+
+// ESERICIZIO 2
+
+// let numero = 8;
+
+// while (numero < 130) {
+//     if (numero % 2 === 0) {
+//         console.log(numero)
+//     }
+//     numero = numero + 1;
+// }
+
+
+// do {
+//     if (numero % 2 === 0) {
+//         console.log(numero)
+//     }
+//     numero = numero + 1;
+// } while (numero < 129);
+
+// ESERCIZIO 3
+
+// let numero = 100;
+
+// while (numero >= 0) {
+//     console.log(numero);
+//     numero = numero - 1;
+// }
+
+
+// do {
+//     console.log(numero);
+//     numero = numero - 1;
+// } while (numero >= 0);
+
+// ESERCIZIO 4
+
+
+
+//scivere un ciclo while che si interrompa quando l'utente scrive 'esci'
+//nella finestra di dialogo
+
+
