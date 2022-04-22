6a429bdd75eeb6330b4b3a8692b59633927d4b89
LittleAkira <cinzia.ariotti.10@gmail.com>
2022-04-22 12:13:45 +0200

    add

---
+++ funzioni.js
@@ -0,0 +1,18 @@
+console.log('pippo');
+
+
+
+// TEOREMA DI PITAGORA
+
+const c1t1 = 4;
+const c2t1 = 3;
+
+const c1t1Alla2 = c1t1 ** 2;
+const c2t1Alla2 = c2t1 ** 2;
+
+const sommaQuadrati = c1t1Alla2 + c2t1Alla2;
+
+const it1 = Math.sqrt(sommaQuadrati);
+
+
+console.log(it1)
